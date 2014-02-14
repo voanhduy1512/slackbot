3b88f1888e5c5dd61651d16898f998c7bd4c25f2
Võ Anh Duy <voanhduy1512@live.com>
2014-02-15 00:22:36 +0700

    make UI cleaner

--- app/models/gitlab.rb
+++ app/models/gitlab.rb
@@ -4,35 +4,38 @@ class Gitlab
 
     commit_string = params[:total_commits_count] > 1 ? "commits" : "commit"
 
-    commits = params[:commits].map do |commit|
-      {
-        fallback: "Required text summary of the attachment that is shown by clients that understand attachments but choose not to show them.",
-
-        pretext: "Commit",
-
-        color: "#36a64f",
-
-
-        fields: [
+    fields = params[:commits].map do |commit|
+      [
           {
             title: "#{commit[:message]}",
             value: "<#{commit[:url]}|#{commit[:id]}>",
             short: false
           }
         ]
-      }
     end
+
+
     channel = "#{params[:room]}"
     if params[:is_channel]
       channel = "\##{channel}"
     elsif params[:is_person]
       channel = "@#{channel}"
     end
+
     payload = {
       channel: channel,
       username: "#{params[:botname]}",
       text: "#{params[:user_name]} push #{params[:total_commits_count]} #{commit_string} to #{params[:repository][:name]}",
-      attachments: commits
+      attachments: {
+
+        fallback: "Required text summary of the attachment that is shown by clients that understand attachments but choose not to show them.",
+
+        pretext: "Commit",
+
+        color: "#36a64f",
+
+        fields: fields
+      }
     }
     opts = {:body => payload.to_json}
     HTTParty.post(url, opts)
